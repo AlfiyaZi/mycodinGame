d2e158f08a68eaa9d69f82d12c9708345c9f10d6
alya <alfiya79@gmail.com>
2017-12-24 17:26:21 +0500

    medium add

---
+++ medium/stock_exchange_losses.py
@@ -0,0 +1,32 @@
+mport sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = int(input())
+values_for_stock = []
+for i in input().split():
+    v = int(i)
+    values_for_stock.append(v)
+
+# Write an action using print
+# To debug: print >> sys.stderr, "Debug messages..."
+
+max_loss = 0
+current_max = 0
+current_min = 0
+for j in range(n):
+    value = int(values_for_stock[j])
+    if value > current_max:
+        if current_max - current_min > max_loss:
+            max_loss = current_max - current_min
+        current_max = value
+        current_min = value
+    elif value < current_min:
+        current_min = value
+
+    if current_max - current_min > max_loss:
+        max_loss = current_max - current_min
+
+print (-max_loss)
